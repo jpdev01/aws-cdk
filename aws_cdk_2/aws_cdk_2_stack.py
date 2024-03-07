31c5a0b559eceff25dc2b448e466525429969903
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-03-06 22:48:55 -0300

    creating lambda

--- aws_cdk_2/aws_cdk_2_stack.py
+++ aws_cdk_2/aws_cdk_2_stack.py
@@ -6,6 +6,8 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_lambda as lambda_,
+    aws_lambda_event_sources as lambda_event_resources,
 )
 
 
@@ -14,13 +16,27 @@ class AwsCdk2Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # create queue
         queue = sqs.Queue(
             self, "AwsCdk2Queue",
             visibility_timeout=Duration.seconds(300),
         )
-
+        # create topic
         topic = sns.Topic(
             self, "AwsCdk2Topic"
         )
 
         topic.add_subscription(subs.SqsSubscription(queue))
+
+        # create lambda
+        sqs_lambda = lambda_.Function(self, "SQSLambda", 
+                                      handler='lambda_handler.handler',
+                                      runtime=lambda_.Runtime.PYTHON_3_10,
+                                      code=lambda_.Code.from_asset('lambda')
+                                      )
+        
+        # create Event Source
+        sqs_event_source = lambda_event_resources.SqsEventSource(queue)
+
+        # Add SQS event source to lambda
+        sqs_lambda.add_event_source(sqs_event_source)
